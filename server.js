17a5df8d1be3573a1d9d8ec5f4abd808d165089e
Marcin Paszt <35789694+marcinpaszt@users.noreply.github.com>
2018-07-26 18:55:15 -0400

    final commit

--- server.js
+++ server.js
@@ -9,17 +9,16 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./app/public/home.html"))
+    res.sendFile(path.join(__dirname, "/app/public/home.html"))
 });
 
 app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "./app/public/survey.html"))
+    res.sendFile(path.join(__dirname, "/app/public/survey.html"))
 });    
-
-
-require('./app/routing/apiRoutes.js')(app);
-require('./app/routing/htmlRoutes.js')(app);
 
 app.listen(PORT, function () {
     console.log('App listening on PORT: ' + PORT)
